59e3c487a67f34fb4220316e0fd29bb463c9f72e
Ashley Margallo <amarie921@yahoo.com>
2018-02-14 17:15:23 -0700

    current

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*
+1. Add jQuery to your project
+2. User selects rock, paper, or scissors
+3. Computer randomly chooses
+  hint: have your choices in an array and randomly choose
+4. DISPLAY the user and computers choice on the screen
+5. DISPLAY the winner
+    - Rock destroys scissors, Scissors cut paper, Paper covers rock.
+ */
+
+/* $(selector).action() 
+    selector - html element by name, id, classes, types, attributes, values of attributes, etc. 
+    action - what to do with the element */
+$(document).ready( function() {
+  //methods code go here
+});
+
+var user;
+var computer;
+var resul;
+
+function startGame
+function keepScore
+function resetGame
+
